2f995a0c024616ad2062c395ca65b69b4a8ca6a3
SamaulHaque <samaulhaque749@gmail.com>
2022-09-30 21:30:52 +0600

    Difference Between State and Props

--- src/components/QuesAndAns/QuesAndAns.js
+++ src/components/QuesAndAns/QuesAndAns.js
@@ -36,11 +36,18 @@ const QuesAndAns = () => {
   <div class="accordion-item">
     <h2 class="accordion-header" id="flush-headingThree">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
-        Accordion Item #3
+        <b>What is the purpose of useEffect hook.</b>
       </button>
     </h2>
     <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
-      <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application.</div>
+      <div class="accordion-body">
+            1. Making asynchronous API calls for data. <br />
+            2. Using Multiple useEffect to Separate Concerns. <br />
+            3. Re-Rendering React Functional Component. <br />
+            4. Setting a subscription to an observable.<br />
+            5. Manually updating the DOM element.<br />
+            6. Updating global variables from inside a function.
+      </div>
     </div>
   </div>
 </div>
